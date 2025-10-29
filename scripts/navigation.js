4873bdda72e99590d3ec0beabadd8c1e8ac32d3f
DollEunAnn <euniceannhdollete@gmail.com>
2025-10-29 22:07:55 +0800

    js function for coures filtering

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,7 @@
 // Declare the elements
 const navbutton = document.querySelector('#ham-btn');
 const navBar = document.querySelector('#nav-bar');
+const pageTitle = document.querySelectorAll('#nav-bar a');
 
 // Toggle for show and hide navbutton
 navbutton.addEventListener('click', () => {
