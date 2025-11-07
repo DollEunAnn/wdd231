792cfeb42eaa5d7b2548d30d4d719df84e47cdf8
DollEunAnn <euniceannhdollete@gmail.com>
2025-11-07 20:30:54 +0800

    updated the business details and finised responsive view for mobile and large screen, separated the css for directory

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -19,25 +19,31 @@ const displayMembers = (members) => {
     const name = document.createElement('h2');
     const portrait = document.createElement('img');
     const address = document.createElement('p');
-    const products = document.createElement('p');
+    const telephone = document.createElement('p');
+    const website = document.createElement('a');
 
     // populate
     name.textContent = `${member.name}`;
     portrait.setAttribute('src', `${member.imageUrl}`);
     address.textContent = `Address: ${member.addresses}`;
-    products.textContent = `Membership Level: ${member.membership_level}`;
+    telephone.textContent = `${member.phone_numbers}`;
+    website.textContent = `${member.website}`;
     
     // append to card
     card.appendChild(portrait);
     card.appendChild(name);
     card.appendChild(address);
-    card.appendChild(products);
+    card.appendChild(telephone);
+    card.appendChild(website);
 
     // build image attribute
     portrait.setAttribute('alt', `Business Image of ${member.name}`);
     portrait.setAttribute('loading', 'lazy');
     portrait.setAttribute('width', '440');
     portrait.setAttribute('height', '440');
+
+    //build link
+    website.href = `${member.website}`;
 
 
     cards.appendChild(card);
