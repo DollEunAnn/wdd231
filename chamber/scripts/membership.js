1e3e572874609afaa182dc09dc002febcc622736
DollEunAnn <euniceannhdollete@gmail.com>
2025-11-19 22:03:28 +0800

    join page , membership modal show function - done, form placement done

---
+++ chamber/scripts/membership.js
@@ -0,0 +1,37 @@
+const dialogBox = document.querySelector('#dialogBox');
+const closeButton = document.querySelector('#closeButton');
+const dialogBoxText = document.querySelector('#dialogBox #content');
+
+const title = document.querySelector('#modal-title');
+
+npButton.addEventListener("click", () => {
+    dialogBox.showModal();
+    title.textContent = "Non Profit Membership";
+    dialogBoxText.innerHTML = `
+    <p> Special Membership Perks </p>`;
+})
+
+bronzeButton.addEventListener("click", () => {
+    dialogBox.showModal();
+    title.textContent = "Bronze Membership";
+    dialogBoxText.innerHTML = `
+    <p> Special Membership Perks </p>`;
+})
+
+silverButton.addEventListener("click", () => {
+    dialogBox.showModal();
+    title.textContent = "Silver Membership";
+    dialogBoxText.innerHTML = `
+    <p> Special Membership Perks </p>`;
+})
+
+goldButton.addEventListener("click", () => {
+    dialogBox.showModal();
+    title.textContent = "Gold Membership";
+    dialogBoxText.innerHTML = `
+    <p> Special Membership Perks </p>`;
+})
+
+closeButton.addEventListener("click", () => {
+    dialogBox.close();
+});
